650494fd88d6cf90286b40971212d62e1752bcf0
Meret Unbehaun <meret.unbehaun@outlook.com>
2022-01-07 13:49:41 +0100

    Start with documentation (Candidate set)

--- Interfaces/DatabaseInterfaces/CandidateSet.py
+++ Interfaces/DatabaseInterfaces/CandidateSet.py
@@ -1,13 +1,37 @@
 class CandidateSet:
+    """
+    database interface for candidates
+    => database columns: input (x, usually a list of properties), items are uniquely identifiable by input), prediction, certainty
+    """
 
-    def add_instance(self, x, y_prediction, accuracy):
+    def add_instance(self, x, y_prediction, certainty):
+        """
+        adds new instance (new last entry) into the candidate database (can be selected for query)
+        :param x: input values
+        :param y_prediction: predicted output value
+        :param certainty: of current model about prediction
+        """
         raise NotImplementedError
 
     def retrieve_all_instances(self):
+        """
+        retrieves all candidates from database (database is left unchanged)
+        :return tuple of numpy arrays: [x], [prediction], [certainty]
+        """
         raise NotImplementedError
 
     def remove_instance(self, x):
+        """
+        remove an candidate based on the input values
+        :param x: input values
+        """
         raise NotImplementedError
 
     def update_instance(self, x, new_y_prediction, new_certainty):
+        """
+        alter the prediction certainty for a candidate (identified by provided input)
+        :param x: input values
+        :param new_y_prediction: the new prediction
+        :param new_certainty: the new certainty about predicition
+        """
         raise NotImplementedError
