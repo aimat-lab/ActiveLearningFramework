3aee5d298636728b9c6e83299caaea6887888584
Meret Unbehaun <meret.unbehaun@outlook.com>
2022-01-12 14:08:42 +0100

    add warning

--- al_components/candidate_update/__init__.py
+++ al_components/candidate_update/__init__.py
@@ -1,2 +1,4 @@
-from .candidate_updater_interface import CandidateUpdater
-from .candidate_updater_initialization import init_candidate_updater
+                                                                        # important to KEEP ORDER!
+
+from .candidate_updater_interface import CandidateUpdater               # 1
+from .candidate_updater_initialization import init_candidate_updater    # 2
--- al_components/query_selection/__init__.py
+++ al_components/query_selection/__init__.py
@@ -1,2 +1,4 @@
-from .query_selector_interface import QuerySelector
-from .query_selector_initialization import init_query_selector
+                                                                            # important to KEEP ORDER!
+
+from .query_selector_interface import QuerySelector                         # 1
+from .query_selector_initialization import init_query_selector              # 2
